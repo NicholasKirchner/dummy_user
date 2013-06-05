ede81895648bf24428ce19622ff363ba03d0846b
Apprentice <apprentice@devbootcamp.com>
2013-06-05 16:23:49 -0700

    Start and Finish User Authentication App

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,43 @@
 get '/' do
-  # Look in app/views/index.erb
+  redirect '/secretpage' if session[:user]
   erb :index
 end
+
+post '/login' do
+  user = User.authenticate(params[:email],params[:password])
+  session[:user] = user
+  redirect '/secretpage'
+end
+
+get '/signup' do
+  @error = session.delete(:error) if session[:error]
+  erb :signup
+end
+
+post '/create' do
+  user = User.new(params)
+  if user.valid? 
+    user.save
+    session[:user] = user
+    redirect '/secretpage'
+  else
+    session[:error] = "Your sign up failed. Password must be between five and twenty characters. Try again."
+    redirect '/signup'
+  end
+  
+end
+
+get '/logout' do
+  session[:user] = nil
+  redirect '/'
+end
+
+get '/secretpage' do
+  @user = session[:user]
+  if @user 
+    erb :secretpage
+  else
+    redirect '/'
+  end
+end
+
---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+# require 'digest/sha1'
+
+
+class User < ActiveRecord::Base
+
+
+  attr_accessor :password
+
+  before_create :encrypt_password
+
+  validates_uniqueness_of :email
+  validates :password, :length => { :in => 5..20 }, :confirmation => true
+  validates :password_confirmation, :presence => true
+
+
+  def self.authenticate(email, password)
+    user = self.find_by_email(email)
+    fresh_encryption = Digest::SHA1.hexdigest("#{user.salt}#{password}")
+    user.encrypted_password == fresh_encryption ? user : nil 
+  end
+
+
+  private
+
+  def encrypt_password
+    self.salt = Digest::SHA1.hexdigest("#{self.email}#{Time.now}")
+    self.encrypted_password = Digest::SHA1.hexdigest("#{self.salt}#{self.password}")
+    self.password = nil
+  end
+
+end
